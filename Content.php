0b87e5599ab1b207de8e3f0e628969e7aa0780ab
OwenHo198 <gocvietgamer@gmail.com>
2022-10-20 18:57:11 +0700

    fix picture

--- Content.php
+++ Content.php
@@ -19,11 +19,11 @@ include_once("connect.php");
 ?>
 <body>
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
+  <!-- <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
+  </ol> -->
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" src="images/ToyStore-OwenStore.png" alt="First slide">
