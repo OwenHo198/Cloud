432273834646ef4bf037820d485b2bc315c06b84
OwenHo198 <gocvietgamer@gmail.com>
2022-10-20 18:55:23 +0700

    fix picture

--- Content.php
+++ Content.php
@@ -28,21 +28,21 @@ include_once("connect.php");
     <div class="carousel-item active">
       <img class="d-block w-100" src="images/ToyStore-OwenStore.png" alt="First slide">
     </div>
-    <div class="carousel-item">
+    <!-- <div class="carousel-item">
       <img class="d-block w-100" src="images/ToyStore-OwenStore.png" alt="Second slide">
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="images/ToyStore-OwenStore.png" alt="Third slide">
-    </div>
+    </div> -->
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+  <!-- <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
   <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
-  </a>
+  </a> -->
 </div>
 <!--<table>
   <tr>
